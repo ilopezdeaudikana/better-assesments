1a341884850e5eaa8d35528b105a0f9649ef99cc
iker <iker@calypsoai.com>
2023-04-28 10:04:49 +0100

    simpler

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import styled from 'styled-components';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Pairs } from './pairs/Pairs';
-import { Posts } from './posts/Posts';
+import React from 'react'
+import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink
+} from 'react-router-dom'
+import { Pairs } from './pairs/Pairs'
+import { Posts } from './posts/Posts'
 
-import './App.css';
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding: 5rem;
-  color: #444;
-`;
+import './App.css'
 
 function App() {
   return (
-      <Router>
-        <div data-testid='App' className='App'>
-          <header className='App-header'>
-            <img src={logo} className="App-logo" alt="logo" />
-            <nav>
-              <ul>
-                <li>
-                  <Link to='/'>Pairs</Link>
-                </li>
-                <li>
-                  <Link to='/posts'>Posts</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <Wrapper>
-            <Routes>
-              <Route path='/' element={<Pairs/>} />
-              <Route path='/posts' element={<Posts/>} />
-              {/* <Route element={Four04Page} />*/}
-            </Routes>
-          </Wrapper>
+    <Router>
+      <div data-testid='App' className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <nav>
+            <ul className='inline-flex rounded-lg my-3 p-1 bg-gray-500 bg-opacity-30 mx-auto'>
+              <li className='mx-1'>
+                <NavLink
+                  className={({ isActive }) =>
+                    [
+                      'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                      isActive ? 'bg-gray-900 text-white' : null
+                    ]
+                      .filter(Boolean)
+                      .join(' ')
+                  }
+                  to='/'
+                >
+                  Pairs
+                </NavLink>
+              </li>
+              <li className='mx-1'>
+                <NavLink
+                  className={({ isActive }) =>
+                  [
+                    'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                    isActive ? 'bg-gray-900 text-white' : null
+                  ]
+                    .filter(Boolean)
+                    .join(' ')
+                }
+                  to='/posts'
+                >
+                  Posts
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div>
+          <Routes>
+            <Route path='/' element={<Pairs />} />
+            <Route path='/posts' element={<Posts />} />
+            {/* <Route element={Four04Page} />*/}
+          </Routes>
         </div>
-      </Router>
-  );
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/pairs/Pairs.tsx
+++ src/pairs/Pairs.tsx
@@ -3,11 +3,7 @@ import { getPartners } from '../common/services/api.service'
 import { groupPartnersByCountry, generateResultsPerCountry } from '../common/services/pairs.service'
 import { Country } from '../common/types/country'
 import { Partner } from '../common/types/partner'
-import styled from 'styled-components'
 
-export const Paragraph = styled.p`
-  text-align: left;
-`
 
 export const Pairs = () => {
   const [data, setData] = useState<{ countries: Country[] }>({
@@ -29,22 +25,20 @@ export const Pairs = () => {
 
   return (
     <Fragment>
-      <ul data-testid='countries'>
+      <ul data-testid='countries' className='w-[30rem] flex flex-col p-3 rounded-xl mx-auto my-2'>
         {data.countries.map(
           (country: Country) =>
             country.attendeeCount > 0 && (
-              <div key={country.name}>
-                <Paragraph>
+              <li key={country.name} className='bg-gray-200 my-2 p-3 rounded-md'>
+                <p>
                   Country: {country.name} | Total: {country.attendeeCount} |
                   When: {country.startDate}
-                </Paragraph>
-                <Paragraph>Attendees</Paragraph>
-                <ul>
-                  {country.attendees.map((attendee: string, i: number) => (
-                    <li key={i}>{attendee}</li>
-                  ))}
-                </ul>
-              </div>
+                </p>
+                <p className='text-left mt-2'>Attendees:</p>
+                <p className='text-left'>
+                  {country.attendees.join(', ')}
+                </p>
+              </li>
             )
         )}
       </ul>
